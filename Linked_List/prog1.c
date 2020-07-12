d580e52468174f9be81575f0de831480e77efb90
sagarp31 <sagarp3199@gmail.com>
2020-07-12 17:21:25 +0530

    Adding a Linked List Programs

---
+++ Linked_List/prog1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+typedef struct Node{
+	int x;
+	struct Node *next;
+}Node;
+
+int main(int argc,char *argv[])
+{
+	Node root,elem2;
+	root.x=15;
+	root.next=&elem2;
+	elem2.x= -2;
+	elem2.next=NULL;
+	printf("First: %d\n",root.x);
+	printf("Second: %d\n",elem2.x);
+
+	printf("Successfully created a linked list....\n");
+	return 0;
+}
